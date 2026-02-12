3380d151386d51c9c11f707a828bfd873ebc7319
Morphes <92577219+Morphess@users.noreply.github.com>
2026-02-12 13:27:09 +0200

    Fix Main class implementation

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package app;
+public class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        DataProvider provider = new DataProvider();
+        String[] data = provider.getData();
+        Corrector corrector = new Corrector();
+        String result = corrector.handleData(data);
+
+        getOutput(result);
+    }
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
